f4bffae00022cf11e0dc5a3f1660b7d2ef098dfe
Goran Flegar <flegar.goran@gmail.com>
2015-10-19 23:18:34 +0200

    Change methods to class methods in vigenere.py

--- src/vigenere.py
+++ src/vigenere.py
@@ -6,38 +6,44 @@ class Vigenere:
 
     _START = ord('A')
 
-    def encode(plaintext, key):
+    @classmethod
+    def encode(cls, plaintext, key):
         """
         Encode _plaintext_ using _key_.
 
         >>> Vigenere.encode('THOMASJEFFERSON', 'PET')
         'ILHBELYIYUIKHSG'
         """
-        return Vigenere._trans(plaintext.upper(), key, Vigenere._encchar)
+        return cls._trans(plaintext.upper(), key, cls._encchar)
 
-    def decode(ciphertext, key):
+    @classmethod
+    def decode(cls, ciphertext, key):
         """
         Decode _ciphertext using _key_.
 
         >>> Vigenere.decode('ILHBELYIYUIKHSG', 'PET')
         'THOMASJEFFERSON'
         """
-        return Vigenere._trans(ciphertext.upper(), key, Vigenere._decchar)
+        return cls._trans(ciphertext.upper(), key, cls._decchar)
 
-    def _trans(text, key, encfunc):
+    @classmethod
+    def _trans(cls, text, key, encfunc):
         k = len(key)
         return ''.join(encfunc(c, key[i % k]) for i, c in enumerate(text))
 
-    def _modad(c, k):
-        c += (1 - 2*(c>0)) * Vigenere._START
-        k += (1 - 2*(k>0)) * Vigenere._START
-        return Vigenere._START + (c + k) % Vigenere._ASIZE
+    @classmethod
+    def _modad(cls, c, k):
+        c += (1 - 2*(c>0)) * cls._START
+        k += (1 - 2*(k>0)) * cls._START
+        return cls._START + (c + k) % cls._ASIZE
 
-    def _encchar(c, k):
-        return chr(Vigenere._modad(ord(c), ord(k)))
+    @classmethod
+    def _encchar(cls, c, k):
+        return chr(cls._modad(ord(c), ord(k)))
 
-    def _decchar(c, k):
-        return chr(Vigenere._modad(ord(c), -ord(k)))
+    @classmethod
+    def _decchar(cls, c, k):
+        return chr(cls._modad(ord(c), -ord(k)))
 
 if __name__ == "__main__":
     import doctest
